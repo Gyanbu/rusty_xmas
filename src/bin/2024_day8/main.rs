b426d6688fe857ba6c113dfe1642a429b1a56d6f
Gyanbu <gyanbu@protonmail.com>
2024-12-08 15:35:21 +0000

    Part 1 done

---
+++ src/bin/2024_day8/main.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use rusty_xmas::Point;
+
+fn main() {
+    let input = rusty_xmas::load_input!();
+    let input: Vec<&str> = input.lines().collect();
+
+    let mut location_ids: HashMap<char, usize> = HashMap::new();
+    {
+        let mut id = 0usize;
+        for c in input.iter().flat_map(|line| line.chars()) {
+            if c != '.' && !location_ids.contains_key(&c) {
+                location_ids.insert(c, id);
+                id += 1;
+            }
+        }
+    }
+    let mut beacons: Vec<Vec<Point>> = vec![Vec::new(); location_ids.len()];
+    for (y, row) in input.iter().enumerate() {
+        for (x, c) in row.chars().enumerate() {
+            if let Some(k) = location_ids.get(&c) {
+                beacons[*k].push(Point::new(x, y));
+            }
+        }
+    }
+    let map_width = input[0].len();
+    let map_height = input.len();
+
+    let mut antinode_map = vec![vec![false; map_width]; map_height];
+    for resonating_beacons in beacons.iter() {
+        for beacon_pair in resonating_beacons.iter().combinations_with_replacement(2).filter(|vec| vec[0] != vec[1]) {
+            let mut vector = beacon_pair[0].vector_between_points(&beacon_pair[1]);
+            if let Some(possible_antinode) = beacon_pair[1].add_vector(&vector) {
+                if possible_antinode.x < map_width && possible_antinode.y < map_height {
+                    antinode_map[possible_antinode.y][possible_antinode.x] = true;
+                }
+            }
+            vector.rotate_180();
+            if let Some(possible_antinode) = beacon_pair[0].add_vector(&vector) {
+                if possible_antinode.x < map_width && possible_antinode.y < map_height {
+                    antinode_map[possible_antinode.y][possible_antinode.x] = true;
+                }
+            }
+        }
+    }
+    // for row in &antinode_map {
+    //     for point in row {
+    //         if *point {
+    //             print!("O");
+    //         } else {
+    //             print!(".");
+    //         }
+    //     }
+    //     println!();
+    // }
+    let answer = antinode_map.iter().flatten().filter(|val| **val).count();
+    println!("Part 1: {}", answer);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,59 @@ macro_rules! load_input {
         std::fs::read_to_string(format!("src/bin/{}/input.txt", module_path!())).unwrap()
     };
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Vector {
+    pub delta_x: isize,
+    pub delta_y: isize,
+}
+
+impl Vector {
+    pub fn new(delta_x: isize, delta_y: isize) -> Vector {
+        Vector { delta_x, delta_y }
+    }
+
+    pub fn rotate_clockwise(&mut self) {
+        // 0, 1 -> -1, 0
+        // -1, 0 -> 0, -1
+        // 0, -1 -> 1, 0
+        // 1, 0 -> 0, 1
+        let delta_x = -self.delta_y;
+        let delta_y = self.delta_x;
+        self.delta_x = delta_x;
+        self.delta_y = delta_y;
+    }
+
+    pub fn rotate_180(&mut self) {
+        self.delta_x = -self.delta_x;
+        self.delta_y = -self.delta_y;
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Point {
+    pub fn default() -> Point {
+        Point { x: 0, y: 0 }
+    }
+
+    pub fn new(x: usize, y: usize) -> Point {
+        Point { x, y }
+    }
+
+    pub fn add_vector(&self, vector: &Vector) -> Option<Point> {
+        let x = self.x.checked_add_signed(vector.delta_x)?;
+        let y = self.y.checked_add_signed(vector.delta_y)?;
+        Some(Point { x, y })
+    }
+
+    pub fn vector_between_points(&self, point: &Point) -> Vector {
+        let delta_x = point.x as isize - self.x as isize;
+        let delta_y = point.y as isize - self.y as isize;
+        Vector::new(delta_x, delta_y)
+    }
+}
