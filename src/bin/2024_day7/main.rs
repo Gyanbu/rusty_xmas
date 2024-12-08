ee23d0ff3d142acc25cc904dca75afa175ed3911
Gyanbu <gyanbu@protonmail.com>
2024-12-08 11:43:56 +0000

    Part 2 done

--- src/bin/2024_day7/main.rs
+++ src/bin/2024_day7/main.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, ops::BitAnd};
+use std::fmt::Display;
 
 #[derive(Debug)]
 struct Equation {
@@ -22,27 +22,49 @@ impl Display for Equation {
 }
 
 impl Equation {
-    fn process_operations(&self, operations_bits: u16) -> usize {
-        let mask = 2u16.pow(self.numbers.len() as u32);
+    fn process_operations(&self, operations_bits: usize) -> usize {
+        let mask = 2usize.pow(self.numbers.len() as u32);
         if operations_bits & mask == mask {
             panic!();
         }
         // println!("{:b}", operations_bits);
         let mut result: usize = self.numbers[0];
         for (n, num) in self.numbers.iter().skip(1).enumerate() {
-            let mask = 2u16.pow(n as u32);
+            let mask = 2usize.pow(n as u32);
             match operations_bits & mask == mask {
                 true => {
                     // println!("1");
                     result *= num;
-                },
+                }
                 false => {
                     // println!("0".);
                     result += num;
-                },
+                }
             }
         }
         // println!();
+        result
+    }
+    
+    fn process_operations_part2(&self, operations: &Vec<usize>) -> usize {
+        let mut result: usize = self.numbers[0];
+        // println!("{:?}", operations);
+        for (n, num) in self.numbers.iter().skip(1).enumerate() {
+            match operations[n] {
+                0 => {
+                    result += num;
+                }
+                1 => {
+                    result *= num;
+                }
+                2 => {
+                    result = result * 10usize.pow(num.ilog10() + 1) + num;
+                    // result = (result.to_string() + &num.to_string()).parse().unwrap();
+                }
+                _ => panic!()
+            }
+        }
+        // println!("{}\n", result);
         result
     }
 }
@@ -70,18 +92,44 @@ fn main() {
     }
 
     let mut answer: usize = 0;
-    'equations: for equation in equations {
-        let DEBUG = equation.test_value;
-        let mask = 2u16.pow(equation.numbers.len() as u32);
-        let mut operations: u16 = 0;
+    for equation in &equations {
+        let mask = 2usize.pow(equation.numbers.len() as u32);
+        let mut operations: usize = 0;
         while operations & mask == 0 {
             let result = equation.process_operations(operations);
             if result == equation.test_value {
                 answer += result;
-                continue 'equations;
+                break;
             }
             operations += 1;
         }
     }
     println!("Part 1: {}", answer);
+
+    let mut answer: usize = 0;
+    for equation in &equations {
+        let mut operations = vec![0usize; equation.numbers.len() - 1];
+        loop {
+            let result = equation.process_operations_part2(&operations);
+            // println!("[{}] {:?} {:?} -> {}", equation.test_value, operations, equation.numbers, result);
+            if result == equation.test_value {
+                answer += result;
+                break;
+            }
+            add_1_to_vec(&mut operations, 3);
+            if operations.iter().all(|op| *op == 0) {
+                break;
+            }
+        }
+    }
+    println!("Part 2: {}", answer);
+}
+
+fn add_1_to_vec(vec: &mut Vec<usize>, base: usize) {
+    for num in vec.iter_mut().rev() {
+        *num = (*num + 1) % base;
+        if *num != 0 {
+            break;
+        }
+    }
 }
